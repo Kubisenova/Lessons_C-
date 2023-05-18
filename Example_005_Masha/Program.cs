0b31a9f220a0c68408cdf445cb777b252999bec2
Aygul <Your>
2023-05-18 23:42:16 +0300

    Example_005_Masha

---
+++ Example_005_Masha/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите своё имя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else 
+{
+  Console.WriteLine("Привет, " + username + "!");  
+}
