75ae944d0521109563c1827810b5dbc4c14935f8
Aygul <Your>
2023-05-19 19:05:21 +0300

    Про_собаку_и_двух_друзей

--- Про_собаку_и_двух_друзей/Program.cs
+++ Про_собаку_и_двух_друзей/Program.cs
@@ -2,13 +2,27 @@
 // // Console.SetCursorPosition(10, 4); // (x, y)
 // // Console.WriteLine("+");
 
-
+// введём обзначения 
 int firstSpeedFriend = 1;
-// int secondSpeedFriend = 2;
-// int dogSpeed = 5;
+int secondSpeedFriend = 2;
+int dogSpeed = 5;
 int distans = 100;
 // int friend = 2;
 // int count = 0;
 
-int resultTimeFirstFriend = distans + firstSpeedFriend;
-Console.WriteLine("Время первого друга равняется ");
+// Найдём скрость сближения двух друзей
+
+int SpeedOfApproach = secondSpeedFriend - firstSpeedFriend;
+Console.WriteLine("Скорость сближения двух друзей =  " + SpeedOfApproach + " м/с");
+
+//  Найдём время через которое два друга втстретятся 
+
+int time = distans / SpeedOfApproach;
+Console.WriteLine("Время через которое два друга встретятся =  " + time + " мин");
+
+//  Теперь найдём расстояние, которое пробежала собака 
+
+int dogS = dogSpeed * time;
+Console.WriteLine("Растояние, которое пробежала собака =  " + dogS + " м/с");
+
+
