a742a91c5bef7facbd269186a610cf77cfb24790
Aygul <Your>
2023-05-20 00:50:09 +0300

    Про_собаку_и_двух_друзей

--- Про_собаку_и_двух_друзей/Program.cs
+++ Про_собаку_и_двух_друзей/Program.cs
@@ -48,49 +48,83 @@
 
 // Решение 2 
 
-Console.Clear();
+// Console.Clear();
 
 // введём обзначения 
+// int firstSpeedFriend = 1;
+// int secondSpeedFriend = 2;
+// int dogSpeed = 5;
+// int distans = 1000;
+// int friend = 2;
+// int count = 0;
+
+
+// // Найдём скрость сближения двух друзей
+
+// int SpeedOfApproach = secondSpeedFriend - firstSpeedFriend;
+// Console.WriteLine("Скорость сближения двух друзей =  " + SpeedOfApproach + " м/с");
+
+// //  Найдём время через которое два друга втстретятся 
+
+// int time = distans / SpeedOfApproach;
+// Console.WriteLine("Время через которое два друга встретятся =  " + time + " сек");
+
+// //  Теперь найдём расстояние, которое пробежала собака 
+
+// int dogS = dogSpeed * time;
+// Console.WriteLine("Растояние, которое пробежала собака =  " + dogS + " м");
+
+// // Теперь найдём сколько именно раз пробежала собака
+
+// while (distans == 1000)
+// {
+//     if (friend == 1)
+//     {
+//         distans = distans - (SpeedOfApproach * time);
+
+//     }
+
+//     if (friend == 2)
+//     {
+//         distans = distans - (SpeedOfApproach * time);
+//     }
+
+//     count++;
+
+//     Console.WriteLine("Собака пробежала между друзьями = " + count + " раз");
+
+// }
+
+
+
+// Решение 3
+
+Console.Clear();
+
 int firstSpeedFriend = 1;
 int secondSpeedFriend = 2;
 int dogSpeed = 5;
-int distans = 1000;
+int distans = 100;
 int friend = 2;
 int count = 0;
 
-
-// Найдём скрость сближения двух друзей
-
-int SpeedOfApproach = secondSpeedFriend - firstSpeedFriend;
-Console.WriteLine("Скорость сближения двух друзей =  " + SpeedOfApproach + " м/с");
-
-//  Найдём время через которое два друга втстретятся 
-
-int time = distans / SpeedOfApproach;
-Console.WriteLine("Время через которое два друга встретятся =  " + time + " сек");
-
-//  Теперь найдём расстояние, которое пробежала собака 
-
-int dogS = dogSpeed * time;
-Console.WriteLine("Растояние, которое пробежала собака =  " + dogS + " м");
-
-// Теперь найдём сколько именно раз пробежала собака
-
-while (distans == 1000)
+while (distans < 10)
 {
     if (friend == 1)
+
     {
-        distans = distans - (SpeedOfApproach * time);
-        
+        int time2 = distans / (secondSpeedFriend + dogSpeed);
+        distans = distans - (secondSpeedFriend + dogSpeed) * time2;
+        count++;
     }
 
     if (friend == 2)
     {
-        distans = distans - (SpeedOfApproach * time);
+        int time1 = distans / (firstSpeedFriend + dogSpeed);
+        distans = distans - (firstSpeedFriend + dogSpeed) * time1;
+        count++;
     }
-
     count++;
 
     Console.WriteLine("Собака пробежала между друзьями = " + count + " раз");
-
 }
