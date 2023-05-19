4bf7d0c96f8bd1823a63d0c390477ac9de127427
Aygul <Your>
2023-05-19 17:48:30 +0300

    Про_собаку_и_двух_друзей

--- Про_собаку_и_двух_друзей/Program.cs
+++ Про_собаку_и_двух_друзей/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.Clear();
+// // Console.SetCursorPosition(10, 4); // (x, y)
+// // Console.WriteLine("+");
+
+
+int firstSpeedFriend = 1;
+// int secondSpeedFriend = 2;
+// int dogSpeed = 5;
+int distans = 100;
+// int friend = 2;
+// int count = 0;
+
+int resultTimeFirstFriend = distans + firstSpeedFriend;
+Console.WriteLine("Время первого друга равняется ");
