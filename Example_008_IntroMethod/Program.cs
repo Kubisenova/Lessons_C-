9abcb39c4ce8c921942379682fce7a33f6a47b31
Aygul <Your>
2023-05-24 21:35:09 +0300

    Example_008_IntroMethod

--- Example_008_IntroMethod/Program.cs
+++ Example_008_IntroMethod/Program.cs
@@ -1,29 +1,34 @@
 ﻿
-Console.Clear();
+// Console.Clear();
 
-int a1 = 15;
-int b1 = 21;
-int c1 = 39;
+// int a1 = 15;
+// int b1 = 21;
+// int c1 = 39;
 
-int a2 = 12;
-int b2 = 23;
-int c2 = 88;
+// int a2 = 12;
+// int b2 = 23;
+// int c2 = 88;
 
-int a3 = 13;
-int b3 = 23;
-int c3 = 33;
+// int a3 = 13;
+// int b3 = 23;
+// int c3 = 33;
 
-int Max(int arg1, int arg2, int arg3)
-{
-    int result = arg1;
-    if (arg2 > result) result = arg2;
-    if (arg3 > result) result = arg3;
-    return result;
-}
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+
+// // int max = Max(max1, max2, max3);
+
+// int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+// Console.WriteLine(max);
 
 // int max = a1;
 // if (b1 > max) max = b1;
@@ -37,6 +42,37 @@ int max = Max(max1, max2, max3);
 // if (b3 > max) max = b3;
 // if (c3 > max) max = c3;
 
-Console.WriteLine(max);
+
+
+
+// Моя практика 
+// Ищу большее из: 
+
+Console.Clear();
+
+int number1 = 105;
+int number2 = 21;
+int number3 = 39;
+int number4 = 302;
+int number5 = 200003;
+int number6 = 33;
+int number7 = 1223;
+int number8 = 23;
+int number9 = 313;
+
+int max = number1;
+if (max < number2) max = number2;
+if (max < number3) max = number3;
+if (max < number4) max = number4;
+if (max < number5) max = number5;
+if (max < number6) max = number6;
+if (max < number7) max = number7;
+if (max < number8) max = number8;
+if (max < number9) max = number9;
+
+{
+    Console.WriteLine(max);
+}
+
 
 
