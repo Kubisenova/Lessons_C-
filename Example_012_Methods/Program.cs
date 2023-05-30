733d8da25b6b494cf63629b30f6aef38f49e425f
Aygul <Your>
2023-05-30 14:57:07 +0300

    Example_012_Methods

--- Example_012_Methods/Program.cs
+++ Example_012_Methods/Program.cs
@@ -84,43 +84,83 @@
 
 // for (int i = 2; i <= 10; i++)
 // {
-//     for (int j = 2; j <= 10; j++)
+//     for (int j = 2; j <= 5; j++)
 //     {
 //         Console.WriteLine($"{i} x {j} = {i * j}");
 //     }
 //     Console.WriteLine("------------");
 // }
 
+
+
+
+
+
+
 //===== Работа с текстом 
 //  Дан текст. В тексте нужно все пробелы заменить черточками, 
 // маленькие буквы “к” заменить большими “К”, 
 // а большие “С” заменить маленькими “с”.
 
-// Ясна ли задача?
+// // Ясна ли задача?
 
+Console.Clear();
 
-string text = "- Я думаю, — сказал князь, улыбаясь, — что,"
-        + "ежели бы вас послали вместо нашего милого Винценгероде,"
-        + "вы бы взяли приступом согласие прусского короля."
-        + "Вы так красноречивы. Вы дадите мне чаю?";
-// string s = "qwerty"
-//             123
-// s[3] // r
+// string text = "- Я думаю, — сказал князь, улыбаясь, — что,"
+//         + "ежели бы вас послали вместо нашего милого Винценгероде,"
+//         + "вы бы взяли приступом согласие прусского короля."
+//         + "Вы так красноречивы. Вы дадите мне чаю?";
 
-string Replase(string text, char oldValue, char newValue)
+// // // string s = "qwerty"
+// // //             123
+// // // s[3] // r
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+//     int lenght = text.Length;
+//     for (int i = 0; i < lenght; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+//     return result;
+// }
+// string newText = Replace (text, ' ', '|');
+
+// Console.WriteLine(newText);
+// Console.WriteLine();
+
+// newText = Replace (text, 'к', 'К');
+// Console.WriteLine(newText);
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+void PrintArray(int[] array)
 {
-    string result = String.Empty;
-    int lenght = text.Length;
-    for (int i = 0; i < Lenght; i++)
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
     {
-        if (text[i] == oldValue) result = result + $"{newValue}";
-        else result = result + $"{text[i]}";
+        Console.Write($"{array[i]}");
     }
-    return result;
+    Console.WriteLine();
 }
-string newText = Replase(text, " ", "|");
+PrintArray(arr);
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
 
-Console.WriteLine(newText);
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
 
-
-
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
